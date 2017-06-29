34325bb1daa8a27d7c39a4e169f6caab6adc4f38
adama_diop_zongo <adamadiopzongo@yahoo.fr>
2017-06-29 13:37:14 -0400

    Cosmetic: Fix identation.

--- src/main/java/com/adz/financialact/common/util/ViewDefinitions.java
+++ src/main/java/com/adz/financialact/common/util/ViewDefinitions.java
@@ -2,13 +2,13 @@ package com.adz.financialact.common.util;
 
 public class ViewDefinitions
 {
-	/***********************************
-	 * VIEW TEMPLATE NAMES & REDIRECTS *
-	 ***********************************/
-	
-	public static final String HOME 			= ("index");
-	public static final String VALUES 			= ("values");
-	public static final String ACT 				= ("act");
-	public static final String STATUS 			= ("status");
-	public static final String SEARCH_VALUES 	= ("search");
+    /***********************************
+     * VIEW TEMPLATE NAMES & REDIRECTS *
+     ***********************************/
+    
+    public static final String HOME             = ("index");
+    public static final String VALUES           = ("values");
+    public static final String ACT              = ("act");
+    public static final String STATUS           = ("status");
+    public static final String SEARCH_VALUES    = ("search");
 }
