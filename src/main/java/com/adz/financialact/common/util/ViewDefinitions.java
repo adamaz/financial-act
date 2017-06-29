a15e061020c5d2f6e530cfaaf1db0e55c5d23169
adama_diop_zongo <adamadiopzongo@yahoo.fr>
2017-06-29 14:26:42 -0400

    Add default template & thymeleaf fragments (header + footer).

--- src/main/java/com/adz/financialact/common/util/ViewDefinitions.java
+++ src/main/java/com/adz/financialact/common/util/ViewDefinitions.java
@@ -7,7 +7,7 @@ public class ViewDefinitions
      ***********************************/
     
     public static final String HOME             = ("index");
-    public static final String VALUES           = ("values");
+    public static final String VALUE           	= ("value");
     public static final String ACT              = ("act");
     public static final String STATUS           = ("status");
     public static final String SEARCH_VALUES    = ("search");
