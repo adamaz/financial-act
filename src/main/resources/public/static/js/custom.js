799a89090dfed152ecefd5cd359d448fa7c524e0
adama_diop_zongo <adamadiopzongo@yahoo.fr>
2017-06-29 13:48:31 -0400

    Add UI & templating resources.

---
+++ src/main/resources/public/static/js/custom.js
@@ -0,0 +1,17 @@
+/**
+ * Copyright, MeymeForms 2017
+ *
+ * custom.js
+ */
+
+
+//-----------------------------------------------------------------------------
+/**
+ * Function that sets the active menu item
+ *
+ * @param menuId Id of the menu item to set as 'active'
+ */
+function fSetActiveMenu(menuId)
+{
+	$('#' + menuId).addClass('active');
+}
